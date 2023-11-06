9fb096ae53ff3d9c919fbbcab26774b402bfea64
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2023-11-06 18:00:08 +0100

    Collapse changed for Details HTML tag

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 export default function Collapse({ title, contentString, contentTable }) {
-    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <>
             {title && (
-                <button onClick={() => setIsOpen(!isOpen)}>
-                    <h2>{title}</h2>
-                    <FontAwesomeIcon icon="fa-solid fa-chevron-up" />
-                </button>
-            )}
-            {isOpen && (
-                <>
-                    {contentString && <p>{contentString}</p>}
-                    {contentTable && contentTable.map((content, index) => (
-                        <p key={`content-${index}`}>{content}</p>
-                    ))}
-                </>
+                <details>
+                    <summary>{title}</summary> {/* TODO Mettre l'icône qui va bien */}
+                    <>
+                        {contentString && <p>{contentString}</p>}
+                        {contentTable && contentTable.map((content, index) => (
+                            <p key={`content-${index}`}>{content}</p>
+                        ))}
+                    </>
+                </details>
             )}
         </>
     )
