3932edf4df8547c6a546239223ebf1ca880d89ab
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2023-11-12 19:01:39 +0100

    Fixes for Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/OC-Projet6-Kasa/",
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: '/',
+    }
+
+    if (command !== 'serve') {
+        config.base = '/OC-Projet6-Kasa/'
+    }
+
+    return config
 })
